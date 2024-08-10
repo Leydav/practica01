cb7d5cc23a4872196e300971401cb5579e2f4f4e
Leydav <160887265+Leydav@users.noreply.github.com>
2024-08-10 13:19:08 -0500

    Práctica 01
    Practica 01 - Seminario - Diseño y Desarrollo de Apps Móviles

---
+++ practica01/Views/Home.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using practica01.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace practica01.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Home2());
+        }
+    }
+}
